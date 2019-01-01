b3bba9dda10faef40c64e46ea89ba65045a74332
Fachrul Kurniansyah <fchrulk@outlook.com>
2019-01-02 02:11:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,12 @@ exec(open("version.py").read())
 
 setup(
     name='LinkedInScraper',
-    version=__version__,                # noqa: F821
+    version=__version__,
     description='Getting data from public LinkedIn Account and still on progress to scrape possible data! ',
-    author='LinkedIn',
+    author='Fachrul Kurniansyah',
     maintainer='Fachrul Kurniansyah',
     maintainer_email='fchrulk@outlook.com',
     url='https://github.com/fchrulk/LinkedInScraper',
-    license='-',
     packages=["LinkedInScraper"],
-    long_description="-",
-    classifiers=[
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
     install_requires=['Unidecode','MechanicalSoup'],
-    tests_require=["coverage"],
 )
