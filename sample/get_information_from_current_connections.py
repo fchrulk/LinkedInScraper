42451bf5af93765b798cb42aac4f790ceb85b2b6
Fachrul Kurniansyah <fchrulk@outlook.com>
2019-01-02 01:53:58 +0700

    Add files via upload

---
+++ sample/get_information_from_current_connections.py
@@ -0,0 +1,15 @@
+from LinkedInScraper import LinkedInScraper
+import argparse
+
+def main():
+	scraper = LinkedInScraper(email=args['email'], password=args["password"])
+	scraper.get_user_connections(complete=False)
+
+if __name__ == "__main__":
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-email", required=True, type=str, help="Input your LinkedIn email to login")
+	ap.add_argument("-password", required=True, type=str, help="Input your LinkedIn password to login")
+	args = vars(ap.parse_args())
+
+	main()
+	
